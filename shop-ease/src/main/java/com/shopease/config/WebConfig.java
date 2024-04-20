50ab6d7fa2819b0922df4d9a83b3bff27ca66468
Victor Eduardo <victorcampos@alu.ufc.br>
2024-04-19 23:26:38 -0300

    Ajusta configurações iniciais equivalentes do web.xml e mapeia filtros.

--- shop-ease/src/main/java/com/shopease/config/WebConfig.java
+++ shop-ease/src/main/java/com/shopease/config/WebConfig.java
@@ -1,10 +1,50 @@
-package com.controller;
+package com.shopease.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import com.shopease.interceptor.logging.LoggingInterceptor;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig {
+@ComponentScan(basePackages = "com.shopease.controller.auth")
+public class WebConfig implements WebMvcConfigurer {
 	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		System.out.println("Entrou em webconfig");
+		registry.addViewController("/").setViewName("forward:/index");
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoggingInterceptor());
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		System.out.println("Passou por addResourceHandlers");
+	    registry.addResourceHandler("/content/**")
+	            .addResourceLocations("/content/");
+	}
+
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/content/pages/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		
+		return resolver;
+	}
 }
