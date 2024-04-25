b6b5c61acc780152c01648ed79ecfbee03706e3e
Victor Eduardo <victorcampos@alu.ufc.br>
2024-04-25 16:35:13 -0300

    Torna uma classe entidade modelo JPA.

--- shop-ease/src/main/java/com/shopease/persistence/model/admin/AdminUser.java
+++ shop-ease/src/main/java/com/shopease/persistence/model/admin/AdminUser.java
@@ -1,19 +1,33 @@
-package com.shopease.controller.system.users.admin;
+package com.shopease.persistence.model.admin;
 
-import com.shopease.controller.system.users.DefaultUser;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-public class AdminUser extends DefaultUser {
+@Entity
+@Table(name = "admin_user_system")
+public class AdminUser {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 	private String uniqueIdentifier;
+	@Column(name = "username")
+	private String username;
+	@Column(name = "password")
+	private String password;
 	
 	public AdminUser() {
 		
 	}
-	
-	public AdminUser(String uniqueIdentifier) {
-		super();
-		this.uniqueIdentifier = uniqueIdentifier;
-	}
 
+	public int getId() {
+		return id;
+	}
+	
 	public String getUniqueIdentifier() {
 		return uniqueIdentifier;
 	}
@@ -22,5 +36,24 @@ public class AdminUser extends DefaultUser {
 		this.uniqueIdentifier = uniqueIdentifier;
 	}
 	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 	
 }
