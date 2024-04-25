a15d3c6ce8d4fb6fa5b629892bf393e556e6b928
Victor Eduardo <victorcampos@alu.ufc.br>
2024-04-25 16:27:53 -0300

    Processa requesições da área do administrador.

--- shop-ease/src/main/java/com/shopease/controller/adminarea/AdminSystem.java
+++ shop-ease/src/main/java/com/shopease/controller/adminarea/AdminSystem.java
@@ -1,18 +1,48 @@
 package com.shopease.controller.adminarea;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.shopease.controller.adminarea.enums.Action;
 import com.shopease.controller.adminarea.enums.Path;
+import com.shopease.persistence.model.usercommon.Client;
+import com.shopease.persistence.repository.CrudRepository;
 
 @Controller
 public class AdminSystem {
 	
+	private CrudRepository<Client, String, Object> clientRegister;
+	
+	@Autowired
+	public AdminSystem(@Qualifier("jpaClientDAO") CrudRepository<Client, String, Object> clientRegister) {
+		this.clientRegister = clientRegister;
+	}
+	
 	@GetMapping(Action.REQ_CLIENT_REGISTER_AREA)
 	public String clientRegisterArea() {
-		System.out.println("Acessou o recurso mapeado.");
 		return Path.REGISTER_CLIENT_AREA.getContent();
+	}
+	
+	@PostMapping(Action.REQ_SUBMIT_CLIENT)
+	public String submitClient(Client client, 
+			@RequestParam("phone_number") String phoneNumber,
+			@RequestParam("home_number") String homeNumber) {
+		client.setHomeNumber(homeNumber);
+		client.setPhoneNumber(phoneNumber);
+		System.out.println(client.toString());
+		clientRegister.save(client);
+		return Path.REGISTER_CLIENT_AREA.getContent();
+	}
+	
+	@GetMapping(Action.REQ_LOAD_LIST_CLIENT)
+	public String loadClientList(Model model) {
+		model.addAttribute("clients", clientRegister.findListObject());
+		return Path.VIEW_CLIENT_LIST.getContent();
 	}
 	
 	@GetMapping(Action.REQ_PRODUCT_REGISTER_AREA)
@@ -24,5 +54,4 @@ public class AdminSystem {
 	public String chatTeamArea() {
 		return Path.CHAT_TEAM_AREA.getContent();
 	}
-	
 }
