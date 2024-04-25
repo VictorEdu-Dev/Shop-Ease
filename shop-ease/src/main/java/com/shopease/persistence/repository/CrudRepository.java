613f3d9ff0ca662810c8854f681b06f58e326092
Victor Eduardo <victorcampos@alu.ufc.br>
2024-04-25 16:28:41 -0300

    Cria o CRUD padrão para qualquer tipo de implementação.

--- shop-ease/src/main/java/com/shopease/persistence/repository/CrudRepository.java
+++ shop-ease/src/main/java/com/shopease/persistence/repository/CrudRepository.java
@@ -1,53 +1,14 @@
-package com.shopease.database.daos;
+package com.shopease.persistence.repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
-import com.shopease.controller.system.users.DefaultUser;
-import com.shopease.database.DatabaseConnection;
-
-public class AuthSessionDAO {
-	private static final String table = "admin_user_system";
+public interface CrudRepository<T, K, V> {
+	public T findById(T arg);
+	public T findByObject(T arg);
+	public List<T> findListObject();
+	public boolean save(T arg);
+	public boolean update(T arg1, Map<K, V> arg2);
+	public boolean delete(T arg);
 	
-	public static boolean subscribeUser(DefaultUser user) {
-		
-		if(!validateCredencials(user)) {
-			try(Connection conn = DatabaseConnection.getConnection();
-		             PreparedStatement stmt = conn.prepareStatement("INSERT INTO " + table +" (name, username, email, phone_number, password) VALUES (?, ?, ?, ?, ?)")) {
-				stmt.setString(1, user.getName());
-				stmt.setString(2, user.getUsername());
-				stmt.setString(3, user.getEmail());
-				stmt.setString(4, user.getPhoneNumber());
-				stmt.setString(5, user.getPassword());
-				int succes = stmt.executeUpdate();
-				
-				if(succes > 0) {
-					return true;
-				} else {
-					return false;
-				}
-			} catch(SQLException e) {
-				e.printStackTrace();
-				return false;
-			}
-		} else {
-			return false;
-		}
-	}
-	
-	public static boolean validateCredencials(DefaultUser user) {
-	     try (Connection conn = DatabaseConnection.getConnection();
-	             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM "+ table +" WHERE username = ? AND password = ?")) {
-	         stmt.setString(1, user.getUsername());
-	         stmt.setString(2, user.getPassword());
-	         try (ResultSet rs = stmt.executeQuery()) {
-	             return rs.next();
-	         }
-	     } catch (SQLException e) {
-	         e.printStackTrace();
-	         return false;
-	     }
-	 }
 }
