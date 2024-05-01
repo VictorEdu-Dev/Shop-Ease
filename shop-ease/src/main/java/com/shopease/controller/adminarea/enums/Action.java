c697ac79e99a5ed5ceb901e59bdd8ec876ee9235
Victor Eduardo <victorcampos@alu.ufc.br>
2024-04-30 22:28:54 -0300

    Acrescenta caminhos para files novos.

--- shop-ease/src/main/java/com/shopease/controller/adminarea/enums/Action.java
+++ shop-ease/src/main/java/com/shopease/controller/adminarea/enums/Action.java
@@ -6,4 +6,9 @@ public class Action {
 	public static final String REQ_CHAT_TEAM_AREA = "/reqChatTeamArea";
 	public static final String REQ_SUBMIT_CLIENT = "/reqSubmitClient";
 	public static final String REQ_LOAD_LIST_CLIENT = "/reqLoadListClient";
+	
+	public static final String REQ_EMPLOYEE_SERVICE_AREA = "/reqEmployeeServiceArea";
+	public static final String REQ_SUMBIT_EMPLOYEE = "/reqSubmitEmployee";
+	public static final String REQ_LOAD_EMPLOYEE_LIST = "/reqLoadEmployeelist";
+	public static final String REQ_ADMIN_GENERAL_SYSTEM = "/reqAdminGeneralSystem";
 }
--- shop-ease/src/main/java/com/shopease/controller/adminarea/enums/Path.java
+++ shop-ease/src/main/java/com/shopease/controller/adminarea/enums/Path.java
@@ -2,9 +2,12 @@ package com.shopease.controller.adminarea.enums;
 
 public enum Path {
 	REGISTER_CLIENT_AREA("register-user-client/register-user-client"),
-	REGISTER_PRODUCT_AREA("register-product/register-products"), 
+	VIEW_CLIENT_LIST("register-user-client/view-table-list"),
+	REGISTER_PRODUCT_AREA("register-product/register-products"),
 	CHAT_TEAM_AREA("web-chat-team/web-chat-team"),
-	VIEW_CLIENT_LIST("register-user-client/view-table-list");
+	EMPLOYEE_AREA_SERVICE("employee-area-service/employee-area-service"),
+	VIEW_EMPLOYEE_LIST("employee-area-service/view-employee-list"), 
+	ADMIN_GENERAL_SYSTEM("login-page-admin/system-admin/system-admin-profile/admin-system");
 	
 	public final String path;
 	
@@ -14,5 +17,13 @@ public enum Path {
 	
 	public String getContent() {
 		return path;
+	}
+	
+	public String getContentForward() {
+		return "forward:/WEB-INF/view/" + path + ".jsp";
+	}
+	
+	public String getContentRedirect() {
+		return "redirect:/WEB-INF/view/" + path + ".jsp";
 	}
 }
